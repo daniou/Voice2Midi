55805f12217c21c2743137e9a05204d2b0dd2e69
daniou <danioliveras7@gmail.com>
2023-08-15 00:25:30 +0200

    added pitch detection

--- scripts/p.py
+++ scripts/p.py
@@ -1,3 +1,122 @@
-# https://prod.liveshare.vsengsaas.visualstudio.com/join?A0340E681F6D3470D0C498D7FED97CC22341
-import pandas as pd
+import librosa
+import numpy as np
+from scipy.fft import fft
+import numpy as np
+from midiutil import MIDIFile
 
+
+class MidiNote:
+    def __init__(self, pitch, start_time, end_time):
+        self.pitch = pitch
+        self.start_time = start_time
+        self.end_time = end_time
+        self.velocity = 100
+
+    def __str__(self):
+        return f"Nota MIDI: {self.pitch}, Tiempo de inicio: {self.start_time}, Tiempo de finalización: {self.end_time}, Velocidad: {self.velocity}"
+
+
+def generate_midi_file(midi_notes, output_file):
+    # Crear un objeto MIDIFile
+    midi_file = MIDIFile(1)  # 1 pista
+
+    # Configurar los parámetros del archivo MIDI
+    tempo = 120  # Tempo en BPM
+    midi_file.addTempo(0, 0, tempo)
+
+    # Agregar las notas al archivo MIDI
+    for note in midi_notes:
+        track = 0  # Pista 0
+        channel = 0  # Canal 0
+        pitch = note.pitch
+        start_time = note.start_time
+        duration = note.end_time - note.start_time
+        velocity = note.velocity
+
+        midi_file.addNote(track, channel, pitch, start_time, duration, velocity)
+
+    # Guardar el archivo MIDI
+    with open(output_file, 'wb') as file:
+        midi_file.writeFile(file)
+
+    # def get_dominant_frequency(audio_data, sr):
+    pass
+    # Calcular la Transformada Rápida de Fourier (FFT)
+
+
+#   fft_result = fft(audio_data)
+
+# Obtener las frecuencias y sus magnitudes
+#  frequencies = np.fft.fftfreq(len(fft_result), 1 / sr)
+# magnitudes = np.abs(fft_result)
+
+# Encontrar la frecuencia predominante
+# dominant_frequency = frequencies[np.argmax(np.abs(magnitudes))]
+
+# return dominant_frequency
+
+def get_dominant_frequency(data, sampling_rate):
+    fft_data = np.fft.fft(data)
+    freqs = np.fft.fftfreq(len(data))
+
+    peak_coefficient = np.argmax(np.abs(fft_data))
+    peak_freq = freqs[peak_coefficient]
+
+    return abs(peak_freq * sampling_rate)
+
+
+def merge_consecutive_notes(midi_notes):
+    merged_notes = []
+
+    if len(midi_notes) == 0:
+        return merged_notes
+
+    current_note = midi_notes[0]
+    for i in range(1, len(midi_notes)):
+        if midi_notes[i].pitch == current_note.pitch and midi_notes[i].start_time == current_note.end_time:
+            current_note.end_time = midi_notes[i].end_time
+        else:
+            merged_notes.append(current_note)
+            current_note = midi_notes[i]
+
+    merged_notes.append(current_note)
+    return merged_notes
+
+
+def frequency_to_midi(frequency):
+    midi_note = 69 + 12 * np.log2(frequency / 440.0)
+    return round(midi_note)
+
+
+def split_audio(file_path, segment_duration):
+    midi_notes = []
+    # Cargar el archivo de audio
+    audio_data, sample_rate = librosa.load(file_path, sr=None)
+    sample_rate /= 2
+    # Calcular el número de segmentos
+    print(int(len(audio_data) / (sample_rate * segment_duration)))
+    num_segments = int(len(audio_data) / (sample_rate * segment_duration))
+
+    # Dividir el archivo de audio en segmentos
+    for i in range(num_segments):
+        start = int(i * sample_rate * segment_duration)
+        end = int((i + 1) * sample_rate * segment_duration)
+        print(start, "adasda", end)
+        segment = audio_data[start:end]
+
+        dominant_frequency = get_dominant_frequency(segment, sample_rate)
+        midi_pitch = frequency_to_midi(dominant_frequency)
+        midi_note = MidiNote(midi_pitch, float(start) / sample_rate, float(end) / sample_rate)
+        print(f"Nota midi: {midi_note}")
+        midi_notes.append(midi_note)
+    return midi_notes
+
+
+file_path = "audio.mp3"
+sample_period = 1.0 / int(input("Introduce los samples por segundo: "))
+print(sample_period)
+midi_notes = split_audio(file_path, sample_period)
+merged_midi_notes = merge_consecutive_notes(midi_notes)
+
+output_file = "output.mid"
+generate_midi_file(merged_midi_notes, output_file)
